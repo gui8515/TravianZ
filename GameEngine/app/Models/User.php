f44ba54fb7230a2b21b9d6ef902b269032eb39b2
Guilherme Gois Pereira <gui8515@gmail.com>
2022-06-18 20:30:38 -0300

    user

--- GameEngine/app/Models/User.php
+++ GameEngine/app/Models/User.php
@@ -14,12 +14,39 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     use Authenticatable, Authorizable, HasFactory;
 
     /**
+     * the table associated with the model
+     * @var string
+     */
+
+    protected $table = 'user';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
     protected $fillable = [
-        'name', 'email',
+        'name',
+        'email',
+        'password',
+        'description',
+        'birthday',
+        'location',
+        'id_tribe',
+        'id_alliance',
+        'is_plus',
+        'is_gold',
+        'is_protected',
+        'is_online',
+        'is_banned',
+        'plus_expires_at',
+        'gold_expires_at',
+        'protect_expires_at',
+        'last_online',
+        'activated_at',
+        'banned_at',
+        'access_level',
+        'remember_token'
     ];
 
     /**
@@ -30,4 +57,16 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    /**
+     * Validation rules for the model
+     * @var array
+     */
+
+    public static $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|between:6,32|confirmed',
+    ];
+
 }
