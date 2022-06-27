d5a9c1a28abde68a773f067ce7703d26b3c5a281
Guilherme Gois Pereira <gui8515@gmail.com>
2022-06-26 23:59:26 -0300

    .

--- GameEngine/app/Http/Controllers/BuildingController.php
+++ GameEngine/app/Http/Controllers/BuildingController.php
@@ -7,78 +7,37 @@ use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Building  $building
-     * @return \Illuminate\Http\Response
-     */
     public function show(Building $building)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\VillageField  $villageField
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(VillageField $villageField)
+    public function edit(Building $building)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\VillageField  $villageField
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, VillageField $villageField)
+    public function update(Request $request, Building $building)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\VillageField  $villageField
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(VillageField $villageField)
+    public function destroy(Building $building)
     {
         //
     }
