eafc8073a6111e8483283d32695122a85747923b
Guilherme Gois Pereira <gui8515@gmail.com>
2026-04-06 21:21:05 -0300

    Refactor mobile village view: Migrate dorf1.php to desktop template, remove mobile-specific styles and scripts, and streamline HTML structure for improved maintainability.

--- dorf1.php
+++ dorf1.php
@@ -28,39 +28,6 @@ if(isset($_GET['newdid'])) {
 	exit;
 } 
 else $building->procBuild($_GET);
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-	<title><?php echo SERVER_NAME . ' - Village overview &raquo; ' . $village->vname; ?></title>
-	<link rel="shortcut icon" href="favicon.ico"/>
-	<link rel="manifest" href="/manifest.json" />
-	<meta name="theme-color" content="#2a2a2a" />
-	<meta http-equiv="cache-control" content="max-age=0" />
-	<meta http-equiv="pragma" content="no-cache" />
-	<meta http-equiv="expires" content="0" />
-	<meta http-equiv="imagetoolbar" content="no" />
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<script src="mt-full.js?0faab" type="text/javascript"></script>
-	<script src="unx.js?f4b7i" type="text/javascript"></script>
-	<script src="new.js?0faab" type="text/javascript"></script>
-	<link href="<?php echo GP_LOCATE; ?>lang/en/compact.css?f4b7i" rel="stylesheet" type="text/css" />
-	<link href="<?php echo GP_LOCATE; ?>lang/en/lang.css?e21d2" rel="stylesheet" type="text/css" />
-	<?php
-	if($session->gpack == null || GP_ENABLE == false) {
-	echo "
-	<link href='".GP_LOCATE."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".GP_LOCATE."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
-	} else {
-	echo "
-	<link href='".$session->gpack."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".$session->gpack."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
-	}
-	?>
-	<script type="text/javascript">
-	window.addEvent('domready', start);
-	</script>
-</head>
 
 /**
  * Detect mobile browsers by inspecting the User-Agent string.
