3ce3694b7c79fcb7b3eb723a4fbb576a06690bda
Guilherme Gois Pereira <gui8515@gmail.com>
2026-04-09 10:45:56 -0300

    feat: enhance mobile village view with responsive design, footer status, and improved header navigation

--- Templates/footer.tpl
+++ Templates/footer.tpl
@@ -17,6 +17,18 @@
 
 ?>
 
+<?php
+$hour = date('Hi');
+$day_night_img = ($hour > 1759 || $hour < 500) ? 'night_image' : 'day_image';
+$show_plus_info = isset($_SESSION['id_user']) && (int)$_SESSION['id_user'] !== 1;
+$plus_active = false;
+$plus_until = 0;
+if ($show_plus_info && isset($session) && isset($session->plus) && isset($session->userinfo['plus'])) {
+	$plus_until = (int)$session->userinfo['plus'];
+	$plus_active = ((int)$session->plus === 1 && time() <= $plus_until);
+}
+?>
+
 <div id="footer">
 <div id="mfoot">
 <div class="footer-menu">
@@ -25,6 +37,28 @@
 <div class="copyright">▶ Server running on <a href="version.php" style="color: #FF5555; text-decoration: none; font-weight: bold;transition: 0.3s;" onmouseover="this.style.color='#FFAA00'" onmouseout="this.style.color='#FF5555'">v.8.4.α ⚡ Quantum Build 42β
 </a>
 </div>
+
+<div id="footer_status">
+	<div class="status_item" title="Day/Night">
+		<img src="img/x.gif" class="<?php echo $day_night_img; ?>" alt="Day/Night" />
+	</div>
+	<div class="status_item">
+		<span>Server time:</span>
+		<strong id="footer_server_time"><?php echo date('H:i:s'); ?></strong>
+	</div>
+	<?php if ($show_plus_info) { ?>
+	<div class="status_item">
+		<span>Plus:</span>
+		<a href="plus.php" class="<?php echo $plus_active ? 'plus_status_active' : 'plus_status_inactive'; ?>">
+			<?php echo $plus_active ? 'Active' : 'Inactive'; ?>
+		</a>
+		<?php if ($plus_active && $plus_until > 0) { ?>
+			<span>(until <?php echo date('Y-m-d H:i', $plus_until); ?>)</span>
+		<?php } ?>
+	</div>
+	<?php } ?>
+</div>
+
 </div>
 </div></center>
 <div id="cfoot">
@@ -54,5 +88,31 @@
 			});
 		});
 	}
+
+	function pad2(num) {
+		return num < 10 ? '0' + num : '' + num;
+	}
+
+	var serverNowTs = <?php echo time(); ?>;
+	var serverUtcOffset = <?php echo (int)date('Z'); ?>;
+	var clientStartMs = Date.now();
+
+	function updateServerClock() {
+		var elapsedSeconds = Math.floor((Date.now() - clientStartMs) / 1000);
+		var serverDate = new Date((serverNowTs + serverUtcOffset + elapsedSeconds) * 1000);
+		var text = pad2(serverDate.getUTCHours()) + ':' + pad2(serverDate.getUTCMinutes()) + ':' + pad2(serverDate.getUTCSeconds());
+		var footerClock = document.getElementById('footer_server_time');
+		if (footerClock) {
+			footerClock.textContent = text;
+		}
+
+		var legacyClock = document.getElementById('tp1');
+		if (legacyClock) {
+			legacyClock.textContent = text;
+		}
+	}
+
+	updateServerClock();
+	setInterval(updateServerClock, 1000);
 })();
 </script>
--- templates/mobile/dorf1.php
+++ templates/mobile/dorf1.php
@@ -10,13 +10,15 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
+
 <head>
 	<title><?php echo SERVER_NAME . ' - Village overview &raquo; ' . $village->vname; ?></title>
-	<link rel="shortcut icon" href="favicon.ico"/>
+	<link rel="shortcut icon" href="favicon.ico" />
 	<meta http-equiv="cache-control" content="max-age=0" />
 	<meta http-equiv="pragma" content="no-cache" />
 	<meta http-equiv="expires" content="0" />
 	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<script src="mt-full.js?0faab" type="text/javascript"></script>
 	<script src="unx.js?f4b7i" type="text/javascript"></script>
@@ -24,77 +26,177 @@
 	<link href="<?php echo GP_LOCATE; ?>lang/en/compact.css?f4b7i" rel="stylesheet" type="text/css" />
 	<link href="<?php echo GP_LOCATE; ?>lang/en/lang.css?e21d2" rel="stylesheet" type="text/css" />
 	<?php
-	if($session->gpack == null || GP_ENABLE == false) {
-	echo "
-	<link href='".GP_LOCATE."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".GP_LOCATE."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
+	if ($session->gpack == null || GP_ENABLE == false) {
+		echo "
+	<link href='" . GP_LOCATE . "travian.css?e21d2' rel='stylesheet' type='text/css' />
+	<link href='" . GP_LOCATE . "lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
 	} else {
-	echo "
-	<link href='".$session->gpack."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".$session->gpack."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
+		echo "
+	<link href='" . $session->gpack . "travian.css?e21d2' rel='stylesheet' type='text/css' />
+	<link href='" . $session->gpack . "lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
 	}
 	?>
 	<script type="text/javascript">
-	window.addEvent('domready', start);
+		window.addEvent('domready', start);
 	</script>
+	<style type="text/css">
+		body,
+		.wrapper {
+			overflow-x: hidden;
+		}
+
+		#dynamic_header,
+		#header,
+		#mid,
+		#footer,
+		#mtop {
+			min-width: 0 !important;
+			width: 100% !important;
+		}
+
+		#dynamic_header,
+		#dynamic_header .dyn1,
+		#dynamic_header .dyn2 {
+			background-position: center top !important;
+		}
+
+		#mid {
+			float: none;
+			display: block;
+			padding: 8px;
+			box-sizing: border-box;
+		}
+
+		#content.village1 {
+			float: none;
+			width: 100% !important;
+			max-width: 720px;
+			padding: 8px !important;
+			margin: 0 auto;
+			box-sizing: border-box;
+		}
+
+		#map_details,
+		#side_info,
+		#side_navi {
+			float: none !important;
+			width: 100% !important;
+			margin: 12px 0 0;
+			position: static !important;
+			left: auto !important;
+			top: auto !important;
+			box-sizing: border-box;
+		}
+
+		#footer #mfoot {
+			width: auto !important;
+			padding-left: 12px;
+			padding-right: 12px;
+			box-sizing: border-box;
+		}
+
+		#res,
+		#ltime {
+			position: static !important;
+			left: auto !important;
+			top: auto !important;
+			width: auto !important;
+			height: auto !important;
+			margin: 8px 0;
+		}
+
+		@media (max-width: 768px) {
+			#content.village1 {
+				padding: 0 !important;
+			}
+
+			#village_map,
+			#village_map img {
+				max-width: 100%;
+			}
+
+			table {
+				display: block;
+				width: 100% !important;
+				overflow-x: auto;
+			}
+		}
+	</style>
 </head>
 
 <body class="v35 ie ie8">
-<div class="wrapper">
-<img style="filter:chroma();" src="img/x.gif" id="msfilter" alt="" />
-<div id="dynamic_header">
-	</div>
-<?php include("Templates/header.tpl"); ?>
-<div id="mid">
-<?php include("Templates/menu.tpl"); ?>
-<div id="content" class="village1">
-<h1><?php echo $village->vname; if($village->loyalty!='100'){ if($village->loyalty>'33'){ $color="gr"; }else{ $color="re"; } ?><div id="loyality" class="<?php echo $color; ?>"><?php echo LOYALTY; ?> <?php echo floor($village->loyalty); ?>%</div><?php } ?></h1>
-<div id="cap" align="left"><?php if($village->capital!='0') { echo "<font color=gray>(".CAPITAL1.")</font>"; } ?></div>
-<?php include("Templates/field.tpl");
-$timer = 1;
-?>
-<div id="map_details">
-<br /><br />
-<?php
-include("Templates/movement.tpl");
-include("Templates/production.tpl");
-include("Templates/troops.tpl");
+	<div class="wrapper">
+		<!-- <img style="filter:chroma();" src="img/x.gif" id="msfilter" alt="" /> -->
+		<div id="dynamic_header">
+		</div>
+		<?php
+		include("Templates/header.tpl");
+		include("Templates/res.tpl");
 
-if($building->NewBuilding) include("Templates/Building.tpl");
-?>
-</div>
-<br /><br /><br /><br /><div id="side_info">
-<?php
-include("Templates/multivillage.tpl");
-include("Templates/quest.tpl");
-include("Templates/news.tpl");
-if(!NEW_FUNCTIONS_DISPLAY_LINKS) {
-	echo "<br><br><br><br>";
-	include("Templates/links.tpl");
-}
-?>
-</div>
-<div class="clear"></div>
-</div>
-<div class="footer-stopper"></div>
-<div class="clear"></div>
+		?>
 
-<?php
-include("Templates/footer.tpl");
-include("Templates/res.tpl");
-?>
-<div id="stime">
-<div id="ltime">
-<div id="ltimeWrap">
-<?php echo CALCULATED_IN;?> <b><?php
-echo round(($generator->pageLoadTimeEnd()-$start_timer)*1000);
-?></b> ms
+		<div id="mid">
+			<?php
+			// include("Templates/menu.tpl"); 
+			?>
+			<div id="content" class="village1">
+				<h1><?php echo $village->vname;
+					if ($village->loyalty != '100') {
+						if ($village->loyalty > '33') {
+							$color = "gr";
+						} else {
+							$color = "re";
+						} ?><div id="loyality" class="<?php echo $color; ?>"><?php echo LOYALTY; ?> <?php echo floor($village->loyalty); ?>%</div><?php } ?></h1>
+				<div id="cap" align="left"><?php if ($village->capital != '0') {
+												echo "<font color=gray>(" . CAPITAL1 . ")</font>";
+											} ?></div>
+				<?php include("Templates/field.tpl");
+				$timer = 1;
+				?>
+				<div id="map_details">
+					<br /><br />
+					<?php
+					include("Templates/movement.tpl");
+					include("Templates/production.tpl");
+					include("Templates/troops.tpl");
 
-<br /><?php echo SERVER_TIME;?> <span id="tp1" class="b"><?php echo date('H:i:s'); ?></span>
-</div>
-	</div>
-</div>
+					if ($building->NewBuilding) include("Templates/Building.tpl");
+					?>
+				</div>
+				<br /><br /><br /><br />
+				<!-- <div id="side_info">
+					<?php
+					include("Templates/multivillage.tpl");
+					include("Templates/quest.tpl");
+					include("Templates/news.tpl");
+					if (!NEW_FUNCTIONS_DISPLAY_LINKS) {
+						echo "<br><br><br><br>";
+						include("Templates/links.tpl");
+					}
+					?>
+				</div> -->
+				<div class="clear"></div>
+			</div>
+			<div class="footer-stopper"></div>
+			<div class="clear"></div>
 
-<div id="ce"></div>
+			<?php
+			include("Templates/footer.tpl");
+			include("Templates/quest.tpl");
+			?>
+			<div id="stime">
+				<div id="ltime">
+					<div id="ltimeWrap">
+						<?php echo CALCULATED_IN; ?> <b><?php
+														echo round(($generator->pageLoadTimeEnd() - $start_timer) * 1000);
+														?></b> ms
+
+						<br /><?php echo SERVER_TIME; ?> <span id="tp1" class="b"><?php echo date('H:i:s'); ?></span>
+					</div>
+				</div>
+			</div>
+
+			<div id="ce"></div>
 </body>
+
 </html>
